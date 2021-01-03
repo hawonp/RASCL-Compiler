cc658721e3be84d3f870b8c00332a10bbd89ca93
Hawon Park <42432872+hawonp@users.noreply.github.com>
2021-01-03 16:24:58 +0900

    Update TestCodeGen.java

--- src/TestCodeGen.java
+++ src/TestCodeGen.java
@@ -1,14 +1,18 @@
+// Author:  Hawon Park
+// Purpose: This file is used to generate intermediate code based on sample RASCL tests provided
+
 import java.io.IOException;
 
 public class TestCodeGen {
 
     public static void main (String [] args){
 
-//        //command line input
-//        String inputFile = args[0];
-//        System.out.println("Command Line Arguement is: " + inputFile);
+       //command line input
+       String inputFile = args[0];
+       System.out.println("Command Line Arguement is: " + inputFile);
+     
+        //change this to a user input, and not a hardcoded address
         String path = "C:\\Users\\hawon\\IdeaProjects\\CodeGeneration\\src\\Testing\\basic_rascl_tests";
-
         String inputFile = path + "\\T31_rascl_test_arrays2.rsc";
 
         //try to generate code output
